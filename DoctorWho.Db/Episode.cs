45469d70245ba34a74e278eef7dd6a5f27a1dd45
sabahb3 <sabahbaara4@gmail.com>
2022-04-14 15:44:02 +0300

    Adding a model called Episode.

---
+++ DoctorWho.Db/Episode.cs
@@ -0,0 +1,14 @@
+namespace DoctorWho.Db;
+
+public class Episode
+{
+    public int EpisodeId { get; set; }
+    public int SeriesNumber { get; set; }
+    public int EpisodeNumber { get; set; }
+    public int EpisodeType { get; set; }
+    public string Title { get; set; }
+    public DateTime EpisodeDate { get; set; }
+    public string Note { get; set; }
+    public int AuthorId { get; set; }
+    public int DoctorId { get; set; }
+}
